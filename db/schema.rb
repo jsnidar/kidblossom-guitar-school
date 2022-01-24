0df6b5649dbcdfc0de3ec5c1d63d730d34db1cff
Jeremiah <jsnidar@gmail.com>
2022-01-24 17:34:13 -0600

    Migrate models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_060807) do
+ActiveRecord::Schema.define(version: 2022_01_24_233308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,71 @@ ActiveRecord::Schema.define(version: 2022_01_24_060807) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_client_accounts_on_user_id"
+  end
+
+  create_table "course_sections", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "meeting_day"
+    t.time "start_time"
+    t.time "end_time"
+    t.integer "status"
+    t.integer "setting"
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_course_sections_on_course_id"
+    t.index ["user_id"], name: "index_course_sections_on_user_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.integer "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "type"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "order_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "client_account_id", null: false
+    t.float "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["client_account_id"], name: "index_orders_on_client_account_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.float "amount"
+    t.bigint "client_account_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["client_account_id"], name: "index_payments_on_client_account_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.integer "gender"
+    t.date "birth_date"
+    t.bigint "client_account_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["client_account_id"], name: "index_students_on_client_account_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +106,11 @@ ActiveRecord::Schema.define(version: 2022_01_24_060807) do
   end
 
   add_foreign_key "client_accounts", "users"
+  add_foreign_key "course_sections", "courses"
+  add_foreign_key "course_sections", "users"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "client_accounts"
+  add_foreign_key "payments", "client_accounts"
+  add_foreign_key "students", "client_accounts"
 end
