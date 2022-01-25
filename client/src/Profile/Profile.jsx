2a02200935ab2bf607c4d2e627c3b8508fba0fb7
Jeremiah <jsnidar@gmail.com>
2022-01-24 23:12:54 -0600

    Create profile element

---
+++ client/src/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+
+import { Container, Row } from "react-bootstrap";
+const Profile = () => {
+
+  return (
+    <Container>
+      <Row>Hi</Row>
+    </Container>
+  );
+}
+
+export default Profile;
