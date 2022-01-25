8ae17cb03dd10b1771872fd51f83260781fab025
Jeremiah <jsnidar@gmail.com>
2022-01-25 06:25:40 -0600

    Initial commit

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,3 +1,4 @@
 class CourseSerializer < ActiveModel::Serializer
   attributes :id, :name
+  has_many :course_sections
 end
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,4 +1,6 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, :gender, :birth_date
   has_one :client_account
+  has_many :student_sections
+  has_many :course_sections, through: :student_sections
 end
