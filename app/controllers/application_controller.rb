2d0e8436c5a647b08e663f96d47f033adb1e6762
Jeremiah <jsnidar@gmail.com>
2022-01-28 17:39:42 -0600

    Add token handling, authorized and logged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,41 @@
 class ApplicationController < ActionController::API
-  include ActionController::Cookies
+  before_action :authorized
 
+  def encode_token(payload)
+    JWT.encode(payload, ENV['JWT_SECRET'])
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # headers: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, ENV['JWT_SECRET'], true, algorithm: ENV['JWT_ALGORITHM'])
+        # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decoded_token
+      # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
+      # or nil if we can't decode the token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
 end
