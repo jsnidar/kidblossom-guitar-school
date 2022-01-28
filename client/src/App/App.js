fd3373402b562b0784a28c56d97264c8532b3092
Jeremiah <jsnidar@gmail.com>
2022-01-28 17:34:36 -0600

    Add signup, login and logout using jwt tokens

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,21 +1,63 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Profile from '../features/user/Profile';
+import NavBar from '../features/navigation/NavBar';
+import SignUp from '../features/user/SignUp';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { userAdded } from '../features/user/usersSlice';
+import { headers, getToken } from '../Globals';
 
 function App() {
+  const [ currentUser, setCurrentUser ] = useState({})
+  const [ loggedIn, setLoggedIn ] = useState(false)
 
+  const logIn = (user) => {
+    setCurrentUser(user)
+    setLoggedIn(true)
+  }
 
+  const logOut = () => {
+    setCurrentUser({});
+    setLoggedIn(false);
+    localStorage.removeItem('jwt');
+  }
+
+  console.log(currentUser)
+  useEffect(() => {
+    const token = localStorage.getItem('jwt')
+    if(token && !loggedIn) {
+      fetch('/current-user', {
+        method: "GET",
+        headers: {
+          ...headers,
+          ...getToken()
+        }
+      })
+        .then(resp => resp.json())
+        .then(user => logIn(user))
+    }
+  },[loggedIn])
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route 
-          path='/' 
-          element={
-            <Profile />
-          } 
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <NavBar logOut={logOut} currentUser={currentUser} />
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path='/' 
+            element={
+              <Profile />
+            } 
+          />
+          <Route 
+            path='/signup' 
+            element={
+              <SignUp logIn={logIn} />
+            } 
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
 
   );
 }
