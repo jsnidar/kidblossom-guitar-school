ee921d534f0dd1f1862e74435fd9a8f57adfeed4
Jeremiah <jsnidar@gmail.com>
2022-01-28 19:50:44 -0600

    Display account information

--- client/src/features/user/Profile.jsx
+++ client/src/features/user/Profile.jsx
@@ -1,11 +1,29 @@
 
 import { Container, Row } from "react-bootstrap";
-const Profile = () => {
+import LogIn from "./LogIn";
+
+const Profile = ({ currentUser, logIn }) => {
 
   return (
     <Container>
-      <Row>Change</Row>
-      <Row>Row 2</Row>
+    { currentUser.id ?
+      <>
+        <Row className='pt-2'>
+          <h1>Welcome {currentUser.first_name} {currentUser.last_name}</h1>
+        </Row>
+        <Row>
+          <h2>Account Information</h2>
+        </Row>
+        <Row className='border p-1'>
+          <h3 className='border-bottom'>Contact Information</h3>
+          <p>Address: {currentUser.address}, {currentUser.city}, {currentUser.state}, {currentUser.zip_code}</p>
+          <p>Phone: {currentUser.primary_phone}</p>
+          <p>Email: {currentUser.primary_email}</p>
+        </Row>
+        <Row>
+        </Row>  
+      </>
+      : <LogIn logIn={logIn} /> }
     </Container>
   );
 }
