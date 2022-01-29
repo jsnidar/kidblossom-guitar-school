0ec2e89967a76133ff39dd0d35c4403ccd9f8fad
Jeremiah <jsnidar@gmail.com>
2022-01-28 19:51:07 -0600

    Create login form with error messages

--- client/src/features/user/LogIn.js
+++ client/src/features/user/LogIn.js
@@ -1,9 +1,87 @@
-import { Container, Row } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Form, Container, Row, Col, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import ErrorAlert from '../../App/ErrorAlert';
+import { headers } from '../../Globals';
 
-const LogIn = () => {
+const LogIn = ({ logIn }) => {
+
+  let navigate = useNavigate()
+  const [errors, setErrors] = useState(null)
+  const [formData, setFormData ] = useState({
+    password: '',
+    primary_email: ''
+  })
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  const handleLogInSubmit = e => {
+    e.preventDefault();
+
+    const strongParams = {
+      ...formData
+    }
+
+    fetch('/login', {
+      method: "POST",
+      headers,
+      body: JSON.stringify(strongParams)
+    })
+      .then(res => {
+        if(res.ok) {
+          res.json()
+          .then(data => {
+            logIn(data.user);
+            localStorage.setItem('jwt', data.token)
+            navigate('/')
+          })
+        }else{
+          res.json().then(e => setErrors(e))
+        }
+      })
+    }
 
   return (
     <Container>
+      <Form className='p-2'>
+        <Row>
+          <h1>Log In</h1>
+          { errors ? <ErrorAlert errors={errors.errors} /> : null }
+        </Row>
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="primary_email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control 
+                type="text" 
+                placeholder=""
+                value={formData.primary_email}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control 
+              type="password" 
+              placeholder="Enter password" 
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          </Col>
+        </Row>
+        <Button 
+          variant="warning" 
+          type="submit"
+          onClick={e => handleLogInSubmit(e)}
+        >
+          Submit
+        </Button>
+      </Form> 
     </Container>
   );
 }
