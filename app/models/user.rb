b9855616d6512128d156be03b44cadc500c2d422
Jeremiah <jsnidar@gmail.com>
2022-01-29 17:53:31 -0600

    Remove associations for course_sections

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,4 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :primary_email_confirmation, :address, :city, :state, :zip_code, presence: true
 
   has_one :client_account
-  has_many :course_sections
 end
