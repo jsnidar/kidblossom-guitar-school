e8226b6cc10243995ee3d254452d01a23fe6136f
Jeremiah <jsnidar@gmail.com>
2022-01-28 19:53:02 -0600

    Validate presence and email comfirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   enum role: [:client, :instructor, :admin]
+
+  validates :primary_email, confirmation: true, uniqueness: true
+  validates :first_name, :last_name, :primary_email_confirmation, :address, :city, :state, :zip_code, presence: true
+
   has_one :client_account
   has_many :course_sections
 end
