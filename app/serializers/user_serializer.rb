9c6951386a3f2cbecb0eb6464f31aa0e5988d76d
Jeremiah <jsnidar@gmail.com>
2022-01-28 19:51:29 -0600

    Add primary_email

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :password_digest, :primary_email, :primary_phone, :address, :city, :state, :zip_code, :role
+  attributes :id, :first_name, :last_name, :password_digest, :primary_email, :primary_phone, :address, :city, :state, :zip_code, :role, :primary_email
 end
