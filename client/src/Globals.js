a20c6dd98093367978557acc221dc42d8f61a3b7
Jeremiah <jsnidar@gmail.com>
2022-01-30 18:09:23 -0600

    Add baseUrl

--- client/src/Globals.js
+++ client/src/Globals.js
@@ -3,6 +3,8 @@ export const headers = {
   "Content-Type": "application/json"
 }
 
+export const baseUrl = "http://localhost:3000"
+
 export const getToken = () => {
   return {
     'Authorization': `bearer ${ localStorage.getItem('jwt') }`
