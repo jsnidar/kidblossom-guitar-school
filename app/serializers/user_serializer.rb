667158ab22c41de33fef5e46a9872a53c16e0c77
Jeremiah <jsnidar@gmail.com>
2022-02-01 18:17:03 -0600

    Add students associations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :primary_email, :primary_phone, :address, :city, :state, :zip_code
+  attributes :id, :first_name, :last_name, :primary_email, :primary_phone, :address, :city, :state, :zip_code 
 
   has_one :client_account
+  has_many :students, through: :client_account
+
 end
