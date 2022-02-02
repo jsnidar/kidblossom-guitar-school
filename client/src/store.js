5f302fde8990a19546c661007cbf489d72138abe
Jeremiah <jsnidar@gmail.com>
2022-02-01 18:16:42 -0600

    Add students and errors reducers

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './features/user/userSlice'
+import userReducer from './features/user/userSlice';
+import studentsReducer from './features/students/studentsSlice';
+import errorsReducer from './errorHandling/errorsSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    students: studentsReducer,
+    errors: errorsReducer
   },
 });
 
