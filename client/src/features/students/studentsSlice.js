a1d7993e6beaece723030d4f093e60d734da57bd
Jeremiah <jsnidar@gmail.com>
2022-02-01 18:12:23 -0600

    Create with full CRUD working

---
+++ client/src/features/students/studentsSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const studentsSlice = createSlice({
+  name: 'students',
+  initialState: {
+    entities: [],
+    status: 'idle',
+  },
+  reducers: {
+    studentFetchRejected(state, action) {
+      state.status = "rejected"
+    },
+    studentActionLoading(state, action) {
+      state.status = "loading"
+    },
+    studentsFetched(state, action) {
+      state.entities = action.payload;
+    },
+    studentAdded(state, action) {
+      state.entities.push(action.payload)
+    },
+    studentRemoved(state, action) {
+      state.entities = state.entities.filter(student => student.id !== action.payload)
+    },
+    studentUpdated(state, action) { 
+      const student = state.entities.find((student) => student.id === action.payload.id)
+      if (student) {
+        student.first_name = action.payload.first_name 
+        student.last_name = action.payload.last_name
+        student.birth_date = action.payload.birth_date
+        student.gender = action.payload.gender
+      }
+    }
+  }
+})
+// export the action creators
+export const { studentAdded, studentRemoved, studentUpdated, studentFetchRejected, studentsFetched, studentActionLoading } = studentsSlice.actions;
+
+export default studentsSlice.reducer;
