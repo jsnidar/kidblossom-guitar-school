89841a5d2bec990237be0b15906bb33e15a3e560
Jeremiah <jsnidar@gmail.com>
2022-02-01 18:14:12 -0600

    Create student card with styling and edit student

---
+++ client/src/features/students/StudentCard.jsx
@@ -0,0 +1,69 @@
+
+import { Container, Col, Row, Button } from "react-bootstrap"
+import StudentForm from "./StudentForm"
+import { useState } from "react"
+import { customStyles } from "../../Globals"
+import { useDispatch } from "react-redux"
+import { studentRemoved, studentActionLoading } from "./studentsSlice"
+import { baseUrl, headers, getToken } from "../../Globals";
+
+
+const StudentCard = ({ student }) => {
+
+  const [ editStudent, setEditStudent ] = useState(false)
+  const dispatch = useDispatch()
+
+  const handleRemoveStudent = () => {
+
+    dispatch(studentActionLoading());
+    fetch(baseUrl + `/students/${student.id}`, {
+      method: "DELETE",
+      headers: {
+        ...headers,
+        ...getToken()
+      },
+      body: JSON.stringify(student.id)
+    })
+    .then(dispatch(studentRemoved(student.id)))
+  }
+
+  
+  const displayStudentInfo = <Container>
+    {customStyles}
+    <Row className='border p-1 m-1'>
+      <h3 className='border-bottom'>{student.first_name} {student.last_name}</h3>
+      <Col>
+        Gender: {student.gender}
+        <br></br>
+        {/* Reformat this Jan 12, 2033 */}
+        Birthdate: {student.birth_date}
+      </Col>
+      <Col>
+        
+      </Col>
+      <Row className="justify-content-evenly">
+        <Button 
+          variant='yellow' 
+          onClick={() => setEditStudent(true)}
+        >Edit Student Info</Button>
+        <Button 
+        variant='yellow' 
+        onClick={() => handleRemoveStudent(student.id)} 
+      >Remove Student</Button>
+      </Row> 
+    </Row>
+    
+  </Container>
+
+  return (
+    <> 
+      {editStudent ? <StudentForm 
+          setShowStudentForm={setEditStudent} 
+          studentToEdit={student} 
+        /> : displayStudentInfo
+      } 
+    </>
+  )
+}
+
+export default StudentCard;
