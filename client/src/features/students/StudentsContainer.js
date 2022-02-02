d8156f5e7286cb701a90337951f0fa71bbec9c9b
Jeremiah <jsnidar@gmail.com>
2022-02-01 18:12:59 -0600

    Update to hold students and student form

--- client/src/features/students/StudentsContainer.js
+++ client/src/features/students/StudentsContainer.js
@@ -1,15 +1,36 @@
 import { useState } from "react"
 import { Row, Button } from "react-bootstrap"
 import { customStyles } from "../../Globals"
-import NewStudentForm from "./NewStudentForm"
+import StudentForm from "./StudentForm"
+import StudentCard from "./StudentCard"
+import { useSelector } from "react-redux";
+
 
 const StudentsContainer = () => {
-  const [showNewStudentForm, setShowNewStudentForm] = useState(false)
+
+  const [showStudentForm, setShowStudentForm] = useState(false)
+  const students = useSelector(state => state.students.entities)
+  
+  const renderStudents = students.length > 0 ? students.map(student => <StudentCard 
+      key={student.id}
+      student={student} 
+    />
+  ) : null
+
   return (
-    <Row>
+    <Row className='pt-2'>
     {customStyles}
-      <h3>Students</h3>
-      {showNewStudentForm ? <NewStudentForm /> : <Button variant='yellow' onClick={() => setShowNewStudentForm(!showNewStudentForm)}>Add a Student</Button>}
+    <h2>Students</h2>
+    {renderStudents}
+    {showStudentForm ? <StudentForm 
+        setShowStudentForm={setShowStudentForm} 
+      /> : <Button 
+        variant='yellow' 
+        onClick={() => setShowStudentForm(true)}
+      >
+        Add a Student
+      </Button>
+    }
     </Row>
   )
 }
