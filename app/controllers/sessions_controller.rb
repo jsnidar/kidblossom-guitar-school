3d851433b9156a0c74c1e32053c6418f15465c66
Jeremiah <jsnidar@gmail.com>
2022-02-03 23:46:53 -0600

    Add coures and students to json rendering

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authorized
 
   def get_current_user
-    render json: current_user
+    render json: { user: current_user, courses: current_user.courses, students: current_user.students }
     # retrieve a token and decode that token in order to get the current user
   end
 
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(primary_email: params[:primary_email])
     if @user && @user.authenticate(params[:password])
       @token = encode_token({ user_id: @user.id })
-      render json: { user: @user, token: @token }, status: :ok
+      render json: { user: @user, courses: @user.courses, students: @user.students, token: @token }, status: :ok
     else
       render json: { errors: ["Email and Password must match"] }, status: :unprocessable_entity
 
