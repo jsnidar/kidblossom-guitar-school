ac75d888adcbc142f5b2ef1da6facc14864754f3
Jeremiah <jsnidar@gmail.com>
2022-02-03 23:37:42 -0600

    Add courses reducer

--- client/src/store.js
+++ client/src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './features/user/userSlice';
 import studentsReducer from './features/students/studentsSlice';
 import errorsReducer from './errorHandling/errorsSlice';
+import coursesReducer from './features/courses/coursesSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     students: studentsReducer,
+    courses: coursesReducer,
     errors: errorsReducer
   },
 });
