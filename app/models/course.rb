527c0b912ff6f8cab4d4cd4b343fae72bc665ea9
Jeremiah <jsnidar@gmail.com>
2022-02-03 23:42:26 -0600

    Update associations and validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,20 @@
 class Course < ApplicationRecord
-  has_many :course_sections
-  has_many :users, through: :course_sections
-  has_many :students, through: :course_sections
+  enum name: [:group_lesson_30_min, :group_lesson_15_min, :private_lesson_30_min]
+  enum meeting_day: [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday]
+  enum status: [:current, :completed]
+  enum setting: [:online, :in_person]
+
+  validates :start_time, presence: true
+  validates :name, numericality: { in: 0..2 }
+  validates :meeting_day, numericality: { in: 0..6 }
+  validates :status, numericality: { in: 0..1 }
+  validates :setting, numericality: { in: 0..1 }
+  validates_date :start_date
+  
+  
+  has_many :users
+  has_many :student_courses
+  has_many :students, through: :student_courses
+
+
 end
