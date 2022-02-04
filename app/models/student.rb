9b6eed00a8f29cb0cbaf1e709b9d088a212eda00
Jeremiah <jsnidar@gmail.com>
2022-02-03 23:41:46 -0600

    Update to reflect change in table relations

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,7 @@ class Student < ApplicationRecord
   enum gender: [:female, :male]
   
   belongs_to :client_account
-  has_many :student_sections
-  has_many :course_sections, through: :student_sections
-  has_many :instructors, through: :student_sections, source: :user
+  has_many :student_courses
+  has_many :courses, through: :student_courses
+  has_many :instructors, through: :student_courses, source: :user
 end
