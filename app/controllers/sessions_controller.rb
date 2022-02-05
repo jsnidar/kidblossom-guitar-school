120bca2997c7a60eb8635923c86d3a0bf035a24e
Jeremiah <jsnidar@gmail.com>
2022-02-05 12:59:45 -0600

    Update current_user and login to reflect user role

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,28 @@ class SessionsController < ApplicationController
   skip_before_action :authorized
 
   def get_current_user
-    render json: { user: current_user, courses: current_user.courses, students: current_user.students }
+    
+    if current_user
+      if current_user.role == 'admin'
+        render json: { user: current_user, users: User.all, courses: Course.all, students: current_user.students, token: @token }
+      else
+        render json: { user: current_user, courses: current_user.courses, students: current_user.students, token: @token }
+      end
     # retrieve a token and decode that token in order to get the current user
+    end
   end
 
   def login
     @user = User.find_by(primary_email: params[:primary_email])
     if @user && @user.authenticate(params[:password])
       @token = encode_token({ user_id: @user.id })
-      render json: { user: @user, courses: @user.courses, students: @user.students, token: @token }, status: :ok
+      if @user.role == "admin"
+        render json: { user: @user, users: User.all, courses: Course.all, students: @user.students, token: @token  }, status: :ok
+      else
+        render json: { user: @user, courses: @user.courses, students: @user.students, token: @token  }, status: :ok
+      end
     else
       render json: { errors: ["Email and Password must match"] }, status: :unprocessable_entity
-
     end
   end
 end
