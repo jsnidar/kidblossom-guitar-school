0d6760a0cbc5cba239e108264ae06b1fb677d35a
Jeremiah <jsnidar@gmail.com>
2022-02-05 12:59:59 -0600

    Remove psuedo code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,8 @@ class ApplicationController < ActionController::API
   def decoded_token
     if auth_header
       token = auth_header.split(' ')[1]
-      # headers: { 'Authorization': 'Bearer <token>' }
       begin
         JWT.decode(token, ENV['JWT_SECRET'], true, algorithm: ENV['JWT_ALGORITHM'])
-        # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]      
       rescue JWT::DecodeError
         nil
       end
@@ -25,8 +23,6 @@ class ApplicationController < ActionController::API
 
   def current_user
    if decoded_token
-      # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
-      # or nil if we can't decode the token
       user_id = decoded_token[0]['user_id']
       @user = User.find_by(id: user_id)
     end
