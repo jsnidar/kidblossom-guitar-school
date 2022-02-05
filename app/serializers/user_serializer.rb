c8b308b47e10411eb41244d27fff9e9d9741ea46
Jeremiah <jsnidar@gmail.com>
2022-02-05 12:57:55 -0600

    Add role attribute

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :primary_email, :primary_phone, :address, :city, :state, :zip_code 
+  attributes :id, :first_name, :last_name, :primary_email, :primary_phone, :address, :city, :state, :zip_code, :role 
 
   has_one :client_account
   has_many :students, through: :client_account
