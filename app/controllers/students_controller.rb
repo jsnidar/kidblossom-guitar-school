86ea93c76facde52c82e8d92b608ce77492d632f
Jeremiah <jsnidar@gmail.com>
2022-02-11 16:31:14 -0600

    Update actions to handle full crud

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,41 +3,71 @@ class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :update, :destroy]
 
   def create
-    @student = @user.students.new(student_params)
-    if @student.save  
-      @token = encode_token({ user_id: @user.id })
-      render json: { student: @student, token: @token }, status: :created
+    if current_user.role == 'client'
+      @student = @user.students.new(student_params)
+      if @student.save  
+        render json: @student, status: :created
+      else
+        render json: @student.errors.full_messages, status: :unprocessable_entity
+      end
     else
-      render json: @student.errors.full_messages, status: :unprocessable_entity
+      render json: { errors: ['Method not allowed']}, status: :method_not_allowed
+    end
+  end
+
+  def index
+    if current_user.role == "admin"
+      render json: Student.all
+    elsif current_user.role == "client"
+      render json: current_user.students
+    else
+      # I need to figure out logic to render all students for an instructor
+      render json: { errors: ['Method not allowed']}, status: :method_not_allowed
+    end
+  end
+
+  def show
+    if @student
+        # I need to add logic for instructor to view a student
+      render json: @student
+    else
+      render json: @student.errors.full_messages, status: :not_foundj
     end
   end
 
   def update
     if @student.update(student_params)
-      render json: { student: @student, token: @token }
+      render json: @student
     else
-      render json: @student.errors.full_messages, status: :unprocessable_entity
+      render json: student.errors.full_messages, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @student.destroy
+    student.destroy
   end
 
   private
 
   # I need to rethink this logic for admin and instructor roles
   def set_student
-    @student = @user.students.find(params[:id])
+    if current_user.role == 'admin'
+      @student = Student.find_by(id: params[:id])
+    elsif current_user.role == "client"
+      @student = current_user.students.find_by(id: params[:id])
+    else
+      render json: { errors: ['Method not allowed']}, status: :method_not_allowed
+    end
   end
 
   def student_params
     params.require(:student).permit(
-        :first_name, 
-        :last_name, 
-        :birth_date,
-        :gender,
-        :client_account_id
-      )
-    end
+      :id,
+      :first_name, 
+      :last_name, 
+      :birth_date,
+      :gender,
+      :client_account_id
+    )
+  end
 end
