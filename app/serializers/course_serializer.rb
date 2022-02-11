a0f9365a4f1144c4f425901a5bd9ade879840767
Jeremiah <jsnidar@gmail.com>
2022-02-11 16:28:10 -0600

    Update associations

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,6 +1,8 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_time, :start_date, :status, :setting, :meeting_day
+  attributes :id, :name, :start_time, :start_date, :status, :setting, :meeting_day, :instructor_name, :instructor_id, :course_list_label, :course_level, :students
 
   belongs_to :user
-  has_many :students
+  has_many :student_courses
+  has_many :students, through: :student_courses
+
 end
