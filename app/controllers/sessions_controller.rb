34c34bef72bac28dd9a2ffc31a6ae32840782d7b
Jeremiah <jsnidar@gmail.com>
2022-02-11 16:31:31 -0600

    Update actions to handle full crud

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,14 @@ class SessionsController < ApplicationController
   def get_current_user
     
     if current_user
-      if current_user.role == 'admin'
-        render json: { user: current_user, users: User.all, courses: Course.all, students: current_user.students, token: @token }
-      else
-        render json: { user: current_user, courses: current_user.courses, students: current_user.students, token: @token }
+      # only returning information about the user
+      if current_user.role == "admin"
+        render json: { user: UserSerializer.new(user), token: @token}, status: :ok
+      elsif current_user.role == "instructor"
+        render json: { user: UserSerializer.new(user), token: @token}, status: :ok
+      elsif current_user.role == "client"
+        # the route to courses is different for clients
+        render json: { user: UserSerializer.new(user), token: @token}, status: :ok
       end
     # retrieve a token and decode that token in order to get the current user
     end
@@ -18,12 +22,49 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       @token = encode_token({ user_id: @user.id })
       if @user.role == "admin"
-        render json: { user: @user, users: User.all, courses: Course.all, students: @user.students, token: @token  }, status: :ok
-      else
-        render json: { user: @user, courses: @user.courses, students: @user.students, token: @token  }, status: :ok
+        render json: { user: UserSerializer.new(user), token: @token}, status: :ok
+      elsif @user.role == "instructor"
+        render json: { user: UserSerializer.new(user), token: @token}, status: :ok
+      elsif @user.role == "client"
+        render json: { user: UserSerializer.new(user), token: @token}, status: :ok
       end
     else
       render json: { errors: ["Email and Password must match"] }, status: :unprocessable_entity
     end
   end
+
+  private 
+
+  def user
+    @user
+  end
+
+  def users 
+    User.all
+  end
+
+  def admin_instructors
+    User.where(role: 1)
+  end
+
+  def admin_courses 
+    Course.all
+  end
+
+  def all_students
+    @user.students
+  end
+
+  def user_students
+    @user.students
+  end
+
+  def instructor_courses
+    @user.courses
+  end
+
+  
+
+
 end
+
