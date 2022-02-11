6bbe0e226240a8b78d56e22f1cbda6e5e58e7988
Jeremiah <jsnidar@gmail.com>
2022-02-11 16:12:15 -0600

    Create courses slice

--- client/src/features/courses/coursesSlice.js
+++ client/src/features/courses/coursesSlice.js
@@ -1,12 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { setErrors } from "../../errorHandling/errorsSlice";
+import { headers, baseUrl, getToken } from "../../Globals";
 
-
+export function fetchCourses() {
+  return function (dispatch) {
+    
+    fetch(baseUrl + '/courses', {
+      method: "GET",
+      headers: {
+        ...headers,
+        ...getToken()
+      },
+    })
+    .then(res => {
+      if(res.ok) {
+        res.json()
+        .then(courses => {
+          dispatch(coursesFetched(courses))
+        })
+      }else{
+        res.json().then(errors => {
+          dispatch(setErrors(errors))
+        })
+      }
+    })
+  }
+}
 
 const coursesSlice = createSlice({
   name: 'courses',
   initialState: {
     entities: [],
+    currentCourse: {},
     status: 'idle',
   },
   reducers: {
@@ -19,8 +44,10 @@ const coursesSlice = createSlice({
     coursesFetched(state, action) {
       state.entities = action.payload;
     },
+    courseFetched(state, action) {
+      state.currentCourse = action.payload
+    },
     courseAdded(state, action) {
-      
       state.entities.push({
         name: action.payload.name,
         id: action.payload.id,
@@ -29,7 +56,8 @@ const coursesSlice = createSlice({
         start_date: action.payload.start_date,
         start_time: new Date(action.payload.start_time).toString(),
         status: action.payload.status,
-        user_id: action.payload.user_id
+        user_id: action.payload.user_id,
+        instructor_name: `${action.payload.user.first_name} ${action.payload.user.last_name}`
       })
     },
     courseRemoved(state, action) {
@@ -47,10 +75,11 @@ const coursesSlice = createSlice({
           course.status = action.payload.status
           course.user_id = action.payload.user_id
       }
+    state.currentCourse = action.payload
     }
   }
 })
 // export the action creators
-export const { courseAdded, courseRemoved, courseUpdated, courseFetchRejected, coursesFetched, courseActionLoading } = coursesSlice.actions;
+export const { courseAdded, courseRemoved, courseUpdated, courseFetchRejected, coursesFetched, courseActionLoading, courseFetched } = coursesSlice.actions;
 
 export default coursesSlice.reducer;
