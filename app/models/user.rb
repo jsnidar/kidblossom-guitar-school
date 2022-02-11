8053f06fbf9ec89d06649b355c5ffd5cbca816c3
Jeremiah <jsnidar@gmail.com>
2022-02-11 16:28:39 -0600

    Update validations for update action

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,18 @@ class User < ApplicationRecord
 
   enum role: [:client, :instructor, :admin]
 
-  validates :primary_email, confirmation: true, uniqueness: true
-  validates :first_name, :last_name, :primary_email_confirmation, :address, :city, :state, :zip_code, presence: true
+  validates :primary_email, confirmation: true, uniqueness: true, on: :create
+  validates :first_name, :last_name, :address, :city, :state, :zip_code, presence: true, on: :create 
+  validates :primary_email_confirmation, presence: true, on: :create
 
   has_one :client_account,  dependent: :destroy
   has_many :students, through: :client_account
   has_many :courses
+  has_many :student_courses, through: :courses
+ 
 
+  # def students_courses
+  #   byebug
+  #   if current_user.role == 'client'
+  #     self.students.pluck(:student_courses)
 end
