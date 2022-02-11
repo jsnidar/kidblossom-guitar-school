54a4feba99ecb0da2c80414eed7e1ddad389779b
Jeremiah <jsnidar@gmail.com>
2022-02-11 16:13:13 -0600

    Create course card

--- client/src/features/courses/CourseCard.js
+++ client/src/features/courses/CourseCard.js
@@ -1,17 +1,42 @@
 import { Container, Col, Row, Button } from "react-bootstrap"
-import CourseForm from "./CourseForm"
-import { useState } from "react"
+import { useEffect } from "react"
 import { customStyles, formatDate } from "../../Globals"
 import { useDispatch, useSelector } from "react-redux"
-import { courseRemoved, courseActionLoading } from "./coursesSlice"
-import { baseUrl, headers, getToken } from "../../Globals";
+import { courseRemoved, courseActionLoading, courseFetched, courseAdded } from "./coursesSlice"
+import { setErrors } from "../../errorHandling/errorsSlice"
+import { baseUrl, headers, getToken, capitalizeWord } from "../../Globals"
+import { useNavigate, useParams } from "react-router-dom"
 
 
-const CourseCard = ({ course }) => {
+const CourseCard = () => {
 
-  const [ editCourse, setEditCourse ] = useState(false)
-  const user = useSelector(state => state.user.currentUser)
+  const { classId } = useParams()
+
+  const course = useSelector(state => state.courses.currentCourse)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  useEffect(()=> {
+    fetch(baseUrl + `/courses/${classId}`, {
+      method: "GET",
+      headers: {
+        ...headers,
+        ...getToken()
+      },
+    })
+    .then(res => {
+      if(res.ok) {
+        res.json()
+        .then(courseInfo => {
+          dispatch(courseFetched(courseInfo))
+          dispatch(courseAdded(courseInfo))
+        })
+      }else{
+        res.json().then(errors => {
+          dispatch(setErrors(errors))
+        })
+      }
+    })  },[classId, dispatch])
 
   const handleRemoveCourse = () => {
 
@@ -27,64 +52,65 @@ const CourseCard = ({ course }) => {
     .then(dispatch(courseRemoved(course.id)))
   }
 
-  const timeString = new Date(course.start_time).toLocaleTimeString()
-  const formattedTimeString = timeString.length === 11 ? timeString.slice(0, 5) + timeString.slice(8) : timeString.slice(0, 4) + timeString.slice(7)
+  let timeString
+  let formattedTimeString
+  
+  if(course.id) {
+    timeString = new Date(course.start_time).toLocaleTimeString()
+    formattedTimeString = timeString.length === 11 ? timeString.slice(0, 5) + timeString.slice(8) : timeString.slice(0, 4) + timeString.slice(7)
+  }
   const formatCourseName = (string) => {
     let name = string.split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1))
     name[name.length - 1] = "Minutes"
     return name.join(" ")
   }
 
-  const capitalizeWord = (word) => word.charAt(0).toUpperCase() + word.slice(1)
-
-  const displayCourseInfo = <Container>
-    {customStyles}
-    <Row className='border p-1 m-1'>
-      <h3 className='border-bottom'>{formatCourseName(course.name)}</h3>
-      <Col>
-        <p>
-          Meeting Day: {capitalizeWord(course.meeting_day)}
-        </p>
-        <p>
-          Start Date: {formatDate(course.start_date).toDateString()}
-        </p>
-        <p>
-          Time: {formattedTimeString}
-        </p>
-      </Col>
-      <Col>
-        <p>
-          Instructor: {capitalizeWord(user.first_name)} {capitalizeWord(user.last_name)}
-        </p>
-        <p>
-          Class Setting: {capitalizeWord(course.setting)}
-        </p>
-        <p>
-          Class Status: {capitalizeWord(course.status)}
-        </p>
-      </Col>
-      <Row className="justify-content-evenly">
-        <Button 
-          variant='yellow' 
-          onClick={() => setEditCourse(true)}
-        >Edit Class Info</Button>
-        <Button 
-        variant='yellow' 
-        onClick={() => handleRemoveCourse(course.id)} 
-      >Remove Class</Button>
-      </Row> 
-    </Row>
-    
-  </Container>
-
   return (
-    <> 
-      {editCourse ? <CourseForm 
-          setShowCourseForm={setEditCourse} 
-          courseToEdit={course} 
-        /> : displayCourseInfo
-      } 
-    </>
+    <Container>
+      {customStyles}
+      {course.id ? <Row className='border p-1 m-1'>
+        <h3 className='border-bottom'>{formatCourseName(course.name)}</h3>
+        <Col>
+          <p>
+            Meeting Day: {capitalizeWord(course.meeting_day)}
+          </p>
+          <p>
+            Start Date: {formatDate(course.start_date).toDateString()}
+          </p>
+          <p>
+            Time: {formattedTimeString}
+          </p>
+        </Col>
+        <Col>
+          <p>
+            Instructor: {course.instructor_name}
+          </p>
+          <p>
+            Level: {capitalizeWord(course.course_level)}
+          </p>
+          <p>
+            Class Setting: {capitalizeWord(course.setting)}
+          </p>
+          <p>
+            Class Status: {capitalizeWord(course.status)}
+          </p>
+        </Col>
+        <Row className="justify-content-evenly">
+          <Button 
+            variant='yellow' 
+            onClick={() => navigate(`/classes/${classId}/edit`)}
+          >Edit Class Info</Button>
+          <Button 
+            variant='yellow' 
+            onClick={() => handleRemoveCourse(course.id)} 
+          >Remove Class</Button>
+          <Button 
+            variant='yellow' 
+            onClick={() => navigate('/classes')}
+          >Return to Instructors Page</Button> 
+        </Row>
+      </Row> : null}
+    </Container>
   )
 }
 
