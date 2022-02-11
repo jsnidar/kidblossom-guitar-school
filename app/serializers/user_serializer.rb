d5ae519224c834ddc9230635e53f2cc13cf15a44
Jeremiah <jsnidar@gmail.com>
2022-02-11 16:26:54 -0600

    Update for associations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :primary_email, :primary_phone, :address, :city, :state, :zip_code, :role 
+  attributes :id, :first_name, :last_name, :primary_email, :primary_phone, :address, :city, :state, :zip_code, :role, :client_account, :courses, :students
 
   has_one :client_account
   has_many :students, through: :client_account
+  has_many :student_courses, through: :students 
   has_many :courses
-
+  
 end
