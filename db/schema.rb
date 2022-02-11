e44b4beabc9dfe3019691563a1a48a5f31559046
Jeremiah <jsnidar@gmail.com>
2022-02-11 16:01:06 -0600

    Add level to course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_225254) do
+ActiveRecord::Schema.define(version: 2022_02_09_223240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_225254) do
     t.integer "status"
     t.integer "setting"
     t.bigint "user_id", null: false
+    t.integer "level"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
