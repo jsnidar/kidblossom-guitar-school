b155a0cb15e3b29e735feaa79160bf545e458206
Jeremiah <jsnidar@gmail.com>
2022-02-13 09:38:00 -0600

    Add full_name

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :primary_email, :primary_phone, :address, :city, :state, :zip_code, :role, :client_account, :courses, :students
+  attributes :id, :first_name, :last_name, :full_name, :primary_email, :primary_phone, :address, :city, :state, :zip_code, :role, :client_account, :courses, :students
 
   has_one :client_account
   has_many :students, through: :client_account
