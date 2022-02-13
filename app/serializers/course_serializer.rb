459c8017dc6ebeb3c9b3404ec73ee42cc36fc8d1
Jeremiah <jsnidar@gmail.com>
2022-02-13 09:38:39 -0600

    Add user_id

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,5 +1,5 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_time, :start_date, :status, :setting, :meeting_day, :instructor_name, :instructor_id, :course_list_label, :course_level, :students
+  attributes :id, :name, :start_time, :start_date, :status, :setting, :user_id, :meeting_day, :instructor_name, :instructor_id, :course_list_label, :course_level, :students
 
   belongs_to :user
   has_many :student_courses
