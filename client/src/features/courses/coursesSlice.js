72ef92557518bbb02f81f3b1be4fe6382deedf2a
Jeremiah <jsnidar@gmail.com>
2022-02-15 16:40:43 -0600

    Update actions with status updates and students

--- client/src/features/courses/coursesSlice.js
+++ client/src/features/courses/coursesSlice.js
@@ -1,11 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { setErrors } from "../../errorHandling/errorsSlice";
-import { headers, baseUrl, getToken } from "../../Globals";
+import { headers, getToken } from "../../Globals";
 
-export function fetchCourses() {
+
+// export const getCourses = createAsyncThunk(
+//   'courses/getCourses', 
+//   async () => {
+//   return await fetch('/courses', {
+//     method: "GET",
+//     headers: {
+//       ...headers,
+//       ...getToken()
+//     },
+//   }).then((res) => res.json())
+// })
+
+// export const postCourse = createAsyncThunk(
+//   'courses/postCourse', 
+//   async (strongParams) => {
+//   return await fetch('/course', {
+//     method: "POST",
+//     headers: {
+//       ...headers,
+//       ...getToken()
+//     },
+//     body: JSON.stringify(strongParams)
+//   }).then((res) => res.json())
+// })
+
+export const fetchCourses = () => {
   return function (dispatch) {
-    
-    fetch(baseUrl + '/courses', {
+    dispatch(courseActionLoading())
+    fetch('/courses', {
       method: "GET",
       headers: {
         ...headers,
@@ -17,10 +43,12 @@ export function fetchCourses() {
         res.json()
         .then(courses => {
           dispatch(coursesFetched(courses))
+          dispatch(coursesFetchSucceeded())
         })
       }else{
         res.json().then(errors => {
           dispatch(setErrors(errors))
+          dispatch(courseFetchRejected())
         })
       }
     })
@@ -32,10 +60,14 @@ const coursesSlice = createSlice({
   initialState: {
     entities: [],
     status: 'idle',
+    errors: []
   },
   reducers: {
-    courseFetchRejected(state, action) {
+    coursesFetchRejected(state, action) {
       state.status = "rejected"
+    },
+    coursesFetchSucceeded(state, action) {
+      state.status = "succeeded"
     },
     courseActionLoading(state, action) {
       state.status = "loading"
@@ -44,17 +76,32 @@ const coursesSlice = createSlice({
       state.entities = action.payload;
     },
     courseAdded(state, action) {
+      const { 
+        name, 
+        id, 
+        setting, 
+        meeting_day, 
+        start_date, 
+        start_time, 
+        status, 
+        user_id, 
+        course_level, 
+        user, 
+        students
+      } = action.payload
+
       state.entities = [...state.entities, {
-        name: action.payload.name,
-        id: action.payload.id,
-        setting: action.payload.setting,
-        meeting_day: action.payload.meeting_day,
-        start_date: action.payload.start_date,
-        start_time: new Date(action.payload.start_time).toString(),
-        status: action.payload.status,
-        user_id: action.payload.user_id,
-        level: action.payload.course_level,
-        instructor_name: `${action.payload.user.first_name} ${action.payload.user.last_name}`
+        name: name,
+        id: id,
+        setting: setting,
+        meeting_day: meeting_day,
+        start_date: start_date,
+        start_time: new Date(start_time).toString(),
+        status: status,
+        user_id: user_id,
+        course_level: course_level,
+        instructor_name: `${user.first_name} ${user.last_name}`,
+        students: students
       }]
     },
     courseRemoved(state, action) {
@@ -62,22 +109,82 @@ const coursesSlice = createSlice({
     },
     courseUpdated(state, action) { 
       const course = state.entities.find((course) => course.id === action.payload.id)
+      const { 
+        name, 
+        id, 
+        meeting_day, 
+        setting, 
+        start_date, 
+        start_time, 
+        status, 
+        user_id, 
+        course_level, 
+        course_list_label, 
+        students 
+      } = action.payload
+
       if (course) {
-          course.name = action.payload.name
-          course.id = action.payload.id
-          course.meeting_day = action.payload.meeting_day
-          course.setting = action.payload.setting
-          course.start_date = action.payload.start_date
-          course.start_time = new Date(action.payload.start_time).toString()
-          course.status = action.payload.status
-          course.user_id = action.payload.user_id
-          course.level = action.payload.level
+          course.name = name
+          course.id = id
+          course.meeting_day = meeting_day
+          course.setting = setting
+          course.start_date = start_date
+          course.start_time = new Date(start_time).toString()
+          course.status = status
+          course.user_id = user_id
+          course.course_level = course_level
+          course.course_list_label = course_list_label
+          course.students = students
       }
-    state.currentCourse = action.payload
     }
-  }
+  },
+  // extraReducers(builder) {
+  //   builder
+  //     .addCase(getCourses.pending, (state, action) => {
+  //       state.status = 'loading'
+  //       state.errors = []
+  //     })
+  //     .addCase(getCourses.fulfilled, (state, action) => {
+  //       state.status = 'succeeded'
+  //       state.entities = action.payload
+  //     })
+  //     .addCase(getCourses.rejected, (state, action) => {
+  //       state.status = 'failed'
+  //       state.errors = action.payload
+  //     })
+  //     .addCase(postCourse.pending, (state, action) => {
+  //       state.status = 'loading'
+  //       state.errors = []
+  //     })
+  //     .addCase(postCourse.fulfilled, (state, action) => {
+  //       state.status = 'succeeded'
+  //       const { name, id, setting, meeting_day, start_date, start_time, status, user_id, course_level, user, students} = action.payload
+
+  //       state.entities.push({
+  //         name: name,
+  //         id: id,
+  //         setting: setting,
+  //         meeting_day: meeting_day,
+  //         start_date: start_date,
+  //         start_time: new Date(start_time).toString(),
+  //         status: status,
+  //         user_id: user_id,
+  //         level: course_level,
+  //         instructor_name: `${user.first_name} ${user.last_name}`,
+  //         students: students
+  //       })
+  //     })
+  //     .addCase(postCourse.rejected, (state, action) => {
+  //       state.status = 'failed'
+  //       state.errors = action.payload
+  //     })
+  // }
 })
 // export the action creators
-export const { courseAdded, courseRemoved, courseUpdated, courseFetchRejected, coursesFetched, courseActionLoading, courseFetched } = coursesSlice.actions;
+export const { courseAdded, courseRemoved, courseUpdated, courseFetchRejected, coursesFetched, coursesFetchSucceeded, courseActionLoading, courseFetched } = coursesSlice.actions;
 
 export default coursesSlice.reducer;
+
+export const selectAllCourses = state => state.courses.entities 
+
+export const selectCourseById = (state, classId) => state.courses.entities.find(course => course.id === parseInt(classId))
