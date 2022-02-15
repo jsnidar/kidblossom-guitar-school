01a3b39c9ca994bbc6aab1fae554cbef25421786
Jeremiah <jsnidar@gmail.com>
2022-02-15 16:42:43 -0600

    Create dropdown for students to add

---
+++ client/src/features/courses/AddStudentDropDown.js
@@ -0,0 +1,26 @@
+
+import { Form } from "react-bootstrap";
+import { useSelector } from 'react-redux'
+import { selectAllStudents } from "../students/studentsSlice";
+import { selectAllCourses } from "./coursesSlice";
+
+const AddStudentDropDown = ({ student , updateStudent }) => {
+
+  const students = useSelector(selectAllStudents)
+  
+  const studentOptions = students.length > 0 ? students.map( (student) => <option
+      value={student.id}
+      key={student.id}
+    >{student.full_name}</option>): null
+  
+  return (
+    <Form.Group className="mb-3" controlId="students">
+      <Form.Select value={student.id} onChange={e => updateStudent(e.target.value, student.listId)}>
+        <option>Select a Student</option>
+        {studentOptions}
+      </Form.Select>
+    </Form.Group>
+  )
+}
+
+export default AddStudentDropDown;
