f47493c7a4f078732cbcf02a695e934b27f3fc93
Jeremiah <jsnidar@gmail.com>
2022-02-15 16:43:19 -0600

    Create component, not in use

---
+++ client/src/features/api/apiSlice.js
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  // The cache reducer expects to be added at `state.api` (already default - this is optional)
+  reducerPath: 'api',
+  // All of our requests will have URLs starting with '/fakeApi'
+  baseQuery: fetchBaseQuery({ baseUrl: '/', prepareHeaders: (headers, { getState }) => {
+    const token = localStorage.getItem('jwt')
+
+    // If we have a token set in state, let's assume that we should be passing it.
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`)
+    }
+
+    return headers
+  },
+}),
+  // The "endpoints" represent operations and requests for this server
+  endpoints: builder => ({
+    // The `getPosts` endpoint is a "query" operation that returns data
+    getPosts: builder.query({
+      // The URL for the request is '/fakeApi/posts'
+      query: () => '/courses'
+    })
+  })
+})
+
+// Export the auto-generated hook for the `getPosts` query endpoint
+export const { useGetCoursesQuery } = apiSlice
+
