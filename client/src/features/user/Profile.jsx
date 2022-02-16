3447a0b5f543c04194e501a506f473b33d3139d8
Jeremiah <jsnidar@gmail.com>
2022-02-16 10:15:01 -0600

    Refactor to have its own route

--- client/src/features/user/Profile.jsx
+++ client/src/features/user/Profile.jsx
@@ -1,41 +1,46 @@
 
+import { useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { customStyles } from "../../Globals";
 import StudentsContainer from "../students/StudentsContainer";
+import ContactInformation from "./ContactInformation";
+import { selectUserById, verifyLoggedIn } from "./userSlice";
 
 const Profile = () => {
 
-  const user = useSelector(state => state.user.entities[0])
+  const { userId } = useParams()
+  
+  const user = useSelector(state => selectUserById(state, userId))
+  const userStatus = useSelector(state => state.user.status)
+  const dispatch = useDispatch()
 
+
+  useEffect(() => {
+    if(userStatus === 'idle' && !user) {
+      dispatch(verifyLoggedIn())
+    }
+
+  }, [userStatus, dispatch, user])
 
   return (
-    <Container>
-      {customStyles}
-      <Row className='pt-2'>
-        <h1>Welcome {user.first_name} {user.last_name}</h1>
-      </Row>
-      <Row>
-        <h2>Account Information</h2>
-      </Row>
-      <Row className='border p-1 m-1'>
-        <h3 className='border-bottom'>Contact Information</h3>
-        <p>Address:</p>
-        <Row className='m-2'>
-        <p>{user.address}</p>
-        <p>{user.city}, {user.state}, {user.zip_code}</p>
-        </Row>
-        <p>Phone: {user.primary_phone}</p>
-        <p>Email: {user.primary_email}</p>
-      </Row>
-      {/* <Row className='border p-1 m-1'>
-        <h3 className='border-bottom'>Account Details</h3>
-        <p>Account Status: {user.client_account.status}</p>
-        <p>{user.client_account.receive_notifications ? "I am receiving email notifications": "I am not receiving email notifications"}</p>
-        <p>Current Balance: ${user.client_account.balance}</p>
-      </Row>  */}
-      <StudentsContainer />
-    </Container>
+    <>
+      {user ? <Container>
+        {customStyles}
+          <Row className='pt-2'>
+          <h1>Profile</h1>
+          </Row>
+          <ContactInformation user={user} />
+          { user.role === 'client' ? <Row className='border p-1 m-1'>
+            <h3 className='border-bottom'>Account Details</h3>
+            <p>Account Status: {user.client_account.status}</p>
+            <p>{user.client_account.receive_notifications ? "I am receiving email notifications": "I am not receiving email notifications"}</p>
+            <p>Current Balance: ${user.client_account.balance}</p>
+          </Row> : null}
+          { user.role === 'client' ? <StudentsContainer /> : null }
+      </Container> : <Container></Container> }
+    </>
   );
 }
 
