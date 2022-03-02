55ad3bb4e1f40a562086dbc302362fff3ccbc20c
Jeremiah <jsnidar@gmail.com>
2022-03-02 09:38:02 -0600

    Add index action to controller for studentForm

--- app/controllers/client_accounts_controller.rb
+++ app/controllers/client_accounts_controller.rb
@@ -1,2 +1,10 @@
 class ClientAccountsController < ApplicationController
+
+  def index
+    if current_user.role == "admin" || current_user.role == "instructor"
+      render json: ClientAccount.all
+    else
+      render json: { errors: ['Method not allowed']}, status: :method_not_allowed
+    end
+  end
 end
