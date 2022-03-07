7decdd8893479d35e13825acefc10ba20b921a5b
Jeremiah <jsnidar@gmail.com>
2022-03-06 21:05:55 -0600

    Add student_courses

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,5 +1,5 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :gender, :birth_date, :first_name, :last_name, :client_account_id, :formatted_birthdate, :full_name, :parent_name
+  attributes :id, :student_courses, :gender, :birth_date, :first_name, :last_name, :client_account_id, :formatted_birthdate, :full_name, :parent_name
 
   belongs_to :client_account
 end
