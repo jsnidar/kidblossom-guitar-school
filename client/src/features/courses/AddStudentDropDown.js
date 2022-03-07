cefe281e032ba0862dd7683279907692274b0cd2
Jeremiah <jsnidar@gmail.com>
2022-03-06 21:04:51 -0600

    Update to only show unenrolled students

--- client/src/features/courses/AddStudentDropDown.js
+++ client/src/features/courses/AddStudentDropDown.js
@@ -1,20 +1,28 @@
 
 import { Form } from "react-bootstrap";
-import { useSelector } from 'react-redux'
 import { selectAllStudents } from "../students/studentsSlice";
+import { useSelector } from "react-redux";
 
-const AddStudentDropDown = ({ student , updateStudent }) => {
+const AddStudentDropDown = ({ student , updateStudent, addedStudents }) => {
 
   const students = useSelector(selectAllStudents)
-  
-  const studentOptions = students.length > 0 ? students.map( (student) => <option
-      value={student.id}
-      key={student.id}
-    >{student.full_name}</option>): null
-  
+
+  const studentOptions = students.length > 0 ? students.map((std) => {
+    if(std.id === student.student_id){
+      return <option
+        value={student.student_id}
+        key={student.student_id}
+      >{student.full_name}</option>
+    }else if(!addedStudents.includes(std.id)){
+      return <option
+        value={std.id}
+        key={std.id}
+      >{std.full_name}</option>
+    }
+  }): null
   return (
     <Form.Group className="mb-3" controlId="students">
-      <Form.Select value={student.id} onChange={e => updateStudent(e.target.value, student.listId)}>
+      <Form.Select value={student.student_id} onChange={e => updateStudent(e.target.value, student.listId)}>
         <option>Select a Student</option>
         {studentOptions}
       </Form.Select>
