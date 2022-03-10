2499c1bd11f38bfb277a79685021488bfa0b99d7
Jeremiah <jsnidar@gmail.com>
2022-03-10 14:19:03 -0700

    Move redux state to parent

--- client/src/features/courses/CoursesList.js
+++ client/src/features/courses/CoursesList.js
@@ -7,49 +7,24 @@ import { sortByDay } from "../../Globals";
 import ErrorAlert from "../../errorHandling/ErrorAlert";
 
 
-const CoursesList = () =>  {
-
-  const courses = useSelector(selectAllCourses)
-  const courseStatus = useSelector(state => state.courses.status)
+const CoursesList = ({ courses }) =>  {
 
   let navigate = useNavigate()
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    if (courseStatus === 'idle') {
-      dispatch(fetchCourses())
-    }
-  }, [courseStatus, dispatch])
-
-  let content
-
-  if (courseStatus === 'loading') {
-    content = <Spinner animation="border" variant="warning" />
-  }else if (courseStatus === 'succeeded') {
-    const sortedCourses = [...courses].sort((a, b) => {
-      if (parseInt(sortByDay(a.meeting_day)) === parseInt(sortByDay(b.meeting_day))) {
-        return a.start_time < b.start_time ? -1 : 1
-      }else{
-        return parseInt(sortByDay(a.meeting_day)) < parseInt(sortByDay(b.meeting_day)) ? -1 : 1
-      }
-    })
-    
-    const renderCourses = sortedCourses.map(course => <ListGroup.Item 
-      key={course.id}
-      action onClick={() => navigate(`/classes/${course.id}`)}
-      >
-      {course.course_list_label}
-      </ListGroup.Item>
-    )
-    content = <ListGroup>{renderCourses}</ListGroup>
-  }else if(courseStatus === 'failed'){
-    content = <ErrorAlert />
-  }
-
+  
+  const sortedCourses = [...courses].sort((a, b) => {
+      return a.start_time < b.start_time ? -1 : 1
+  })
+  
+  const renderCourses = sortedCourses.map(course => <ListGroup.Item 
+    key={course.id}
+    action onClick={() => navigate(`/classes/${course.id}`)}
+    >
+    {course.course_list_label}
+    </ListGroup.Item>
+  )
+  
   return (
-    <>
-      {content}
-    </>
+    <ListGroup>{renderCourses}</ListGroup>
   )
 }
 
