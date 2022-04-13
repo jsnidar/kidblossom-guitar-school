8a8e5f427414bda669a2d2d341256e029164a7cb
Jeremiah <jsnidar@gmail.com>
2022-04-13 14:45:34 -0500

    Reformat making each day a column

--- client/src/features/courses/CoursesContainer.js
+++ client/src/features/courses/CoursesContainer.js
@@ -1,4 +1,4 @@
-import { Row, Container, Button, Spinner } from "react-bootstrap"
+import { Row, Col, Container, Button, Spinner } from "react-bootstrap"
 import { customStyles } from "../../Globals"
 import CoursesList from "./CoursesList";
 import { useNavigate } from "react-router-dom";
@@ -56,33 +56,52 @@ const CoursesContainer = () => {
           Add a Class
         </Button>
       </Row>
-      <Row>Monday</Row>
-      <Row className='pt-2'>
-        <CoursesList courses={meetingDays.monday} />
-      </Row>
-      <Row>Tuesday</Row>
       <Row>
-        <CoursesList courses={meetingDays.tuesday} />
+
       </Row>
-      <Row>Wednsday</Row>
-      <Row className='pt-2'>
-        <CoursesList courses={meetingDays.wednesday} />
-      </Row>
-      <Row>Thursday</Row>
       <Row>
-        <CoursesList courses={meetingDays.thursday} />
-      </Row>
-      <Row>Friday</Row>
-      <Row>
-        <CoursesList courses={meetingDays.friday} />
-      </Row>
-      <Row>Saturday</Row>
-      <Row>
-        <CoursesList courses={meetingDays.saturday} />
-      </Row>
-      <Row>Sunday</Row>
-      <Row>
-        <CoursesList courses={meetingDays.sunday} />
+        <Col>
+          <Row>Monday</Row>
+          <Row className='overflow-auto'>
+            <CoursesList courses={meetingDays.monday} />
+          </Row>
+        </Col>
+        <Col>
+          <Row>Tuesday</Row>
+          <Row className='overflow-auto'>
+            <CoursesList courses={meetingDays.tuesday} />
+          </Row>
+        </Col>
+        <Col>
+          <Row>Wednesday</Row>
+          <Row className='pt-2'>
+            <CoursesList courses={meetingDays.wednesday} />
+          </Row>
+        </Col>
+        <Col>
+          <Row>Thursday</Row>
+          <Row>
+            <CoursesList courses={meetingDays.thursday} />
+          </Row>
+        </Col>
+        <Col>
+          <Row>Friday</Row>
+          <Row>
+            <CoursesList courses={meetingDays.friday} />
+          </Row>
+        </Col>
+        <Col>
+          <Row>Saturday</Row>
+          <Row>
+            <CoursesList courses={meetingDays.saturday} />
+          </Row>
+        </Col>
+        <Col>
+          <Row>Sunday</Row>
+          <Row>
+            <CoursesList courses={meetingDays.sunday} />
+          </Row>
+        </Col>
       </Row>
     </>
   }else if(courseStatus === 'failed'){
